4508fc5b193d245d27b7718a759c74bca0244a9c
calebuki <calebswan00@gmail.com>
2023-11-06 23:35:09 -0800

    started adding rekognition stuff into the main

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,12 @@
 import logging
 import json
+import boto3
+from botocore.client import Config
 from customEncoder import CustomEncoder
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
 
 getMethod = "GET"
 healthPath = "/rekognition/health"
@@ -11,12 +14,20 @@ healthPath = "/rekognition/health"
 
 #main handler
 def lambda_handler(event, context):
+
     logger.info(event)
     httpMethod = event['httpMethod']
     path = event['path']
+    image = event['image']
 
     if httpMethod == getMethod and path == healthPath:
-        response = buildResponse(200)
+        # response = buildResponse(200)
+        # yo do i put my code here ?
+        rekognition = boto3.client('rekognition')
+        image_bytes = event['image_bytes']
+        response = rekognition.detect_labels(Image={'Bytes': image_bytes})
+        labels = get_labels(response)
+
     else:
         response = buildResponse(404, 'Not Found')
     
