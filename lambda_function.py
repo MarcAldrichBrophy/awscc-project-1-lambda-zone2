54a02752a423eff3a42d62fe1e6b3640bdbfa94d
calebuki <calebswan00@gmail.com>
2023-11-07 22:30:07 -0800

    changed path for image

--- lambda_function.py
+++ lambda_function.py
@@ -23,7 +23,7 @@ def lambda_handler(event, context):
     if httpMethod == getMethod and path == healthPath:
 
         # yo do i put my code here ?
-        with open('\image.PNG', 'rb') as image_file: #CHANGE THIS DIRECTORY LATER, upload image to S3 Bucket?
+        with open('Amazon S3\Buckets\project-1-code-zone2\image.PNG', 'rb') as image_file:
             image_bytes = base64.b64encode(image_file.read()).decode('utf-8')
 
         rekognition = boto3.client('rekognition')
