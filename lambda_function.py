dbabe39ff01204389ac35ab9426966973381c2e6
calebuki <calebswan00@gmail.com>
2023-11-07 22:41:12 -0800

    s3 download

--- lambda_function.py
+++ lambda_function.py
@@ -22,12 +22,22 @@ def lambda_handler(event, context):
 
     if httpMethod == getMethod and path == healthPath:
 
-        with open('s3://project-1-code-zone2/image.PNG', 'rb') as image_file:
-            image_bytes = base64.b64encode(image_file.read()).decode('utf-8')
+        s3 = boto3.client('s3')
+
+        bucket = 'project-1-code-zone2'
+        key = 'image.PNG'
+
+        s3.download_file(bucket, key, '/tmp/image.PNG')
+
+        with open('/tmp/image.PNG', 'rb') as f:
+            # if base64: image_bytes = base64.b64encode(image_file.read()).decode('utf-8')
+            image_bytes = f.read()
 
         rekognition = boto3.client('rekognition')
         response = rekognition.detect_labels(Image={'Bytes': image_bytes})
         labels = get_labels(response)
+
+        os.remove('/tmp/image.PNG')
 
         return buildResponse(200)
     
