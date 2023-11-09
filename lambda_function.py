c30787764782741a26e6382a18e9c851a04e7f42
calebuki <calebswan00@gmail.com>
2023-11-08 20:19:17 -0800

    labels

--- lambda_function.py
+++ lambda_function.py
@@ -19,27 +19,19 @@ def lambda_handler(event, context):
     logger.info(event)
     httpMethod = event['httpMethod']
     path = event['path']
+    image_path = event['image_path']
 
     if httpMethod == getMethod and path == healthPath:
 
-        s3 = boto3.client('s3')
+        rekognition = boto3.resource('rekognition')
+        response = rekognition.Image(image_path).detect_labels()
+        # labels = get_labels(response)
+        
+        labels = []
+        for label in response['Labels']:
+            labels.append(label['Name'])
 
-        bucket = 'project-1-code-zone2'
-        key = 'image.PNG'
-
-        s3.download_file(bucket, key, '/tmp/image.PNG')
-
-        with open('/tmp/image.PNG', 'rb') as f:
-            # if base64: image_bytes = base64.b64encode(image_file.read()).decode('utf-8')
-            image_bytes = f.read()
-
-        rekognition = boto3.client('rekognition')
-        response = rekognition.detect_labels(Image={'Bytes': image_bytes})
-        labels = get_labels(response)
-
-        os.remove('/tmp/image.PNG')
-
-        return buildResponse(200)
+        return buildResponse(200, labels)
     
     else:
         return buildResponse(404, 'Not Found')
