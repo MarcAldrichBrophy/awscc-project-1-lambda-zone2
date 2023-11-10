65d24ee793efa0dda8866dfac0dd4627638dfec1
calebuki <calebswan00@gmail.com>
2023-11-10 14:03:09 -0800

    base64 decoding

--- lambda_function.py
+++ lambda_function.py
@@ -2,15 +2,19 @@ import logging
 import json
 import boto3
 import base64
+import io
 from botocore.client import Config
 from customEncoder import CustomEncoder
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+s3 = boto3.client('s3')
 
 
 getMethod = "GET"
 healthPath = "/rekognition/health"
+rekognition = boto3.client('rekognition')
+
 
 
 #main handler
@@ -19,18 +23,24 @@ def lambda_handler(event, context):
     logger.info(event)
     httpMethod = event['httpMethod']
     path = event['path']
-    image_path = event['image_path']
+    image_base64 = event['imageBase64']
 
     if httpMethod == getMethod and path == healthPath:
 
-        rekognition = boto3.resource('rekognition')
-        response = rekognition.Image(image_path).detect_labels()
-        # labels = get_labels(response)
-        
-        labels = []
-        for label in response['Labels']:
-            labels.append(label['Name'])
+        #decode
+        image_data = base64.b64decode(image_base64)
 
+        #convert
+        image = Image.open(io.BytesIO(image_data))
+
+        #run rekognition detect_faces API
+        response = rekognition.detect_labels(Image={'Bytes': image_data})
+    
+        labels = get_labels(response)
+        result = {'labels': labels}
+
+        labels_json = json.dumps()
+        
         return buildResponse(200, labels)
     
     else:
